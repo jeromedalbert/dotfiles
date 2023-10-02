e9ed4d8d6c6c0271c242c3f849ba3ec4869cbedf
Jerome Dalbert <jerome.dalbert@gmail.com>
2023-10-02 09:53:50 -0700

    Update

--- .vim/init.vim.lua
+++ .vim/init.vim.lua
@@ -5,19 +5,3 @@ lspconfig.syntax_tree.setup {
   root_dir = lspconfig.util.root_pattern('.streerc'),
   on_attach = lspformat.on_attach
 }
-
--- local util = require 'formatter.util'
--- require('formatter').setup {
---   filetype = {
---     ruby = {
---       function()
---         return {
---           exe = 'stree',
---           args = {
---             'write'
---           },
---         }
---       end
---     },
---   }
--- }
