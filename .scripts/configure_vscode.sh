964cee7a9e3651a7e6b5adf84628bfc593d25dd7
Jerome Dalbert <jerome.dalbert@gmail.com>
2025-02-02 15:33:36 -0800

    Update

--- .scripts/configure_vscode.sh
+++ .scripts/configure_vscode.sh
@@ -5,3 +5,5 @@ ln -sf ~/.dotfiles/.scripts/.vscode/settings.json ~/Library/Application\ Support
 # Cursor
 ln -sf ~/.dotfiles/.scripts/.vscode/keybindings.json ~/Library/Application\ Support/Cursor/User/keybindings.json
 ln -sf ~/.dotfiles/.scripts/.vscode/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
+rm -rf ~/.cursor/extensions
+ln -s ~/.vscode/extensions ~/.cursor/extensions
