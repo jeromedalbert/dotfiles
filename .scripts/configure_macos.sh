1017db1c76e7d629ae21432a710939bf903f19fe
Jerome Dalbert <jerome.dalbert@gmail.com>
2026-07-22 16:46:36 -0700

    Update

--- .scripts/configure_macos.sh
+++ .scripts/configure_macos.sh
@@ -84,6 +84,7 @@ defaults write com.apple.preview NSUserKeyEquivalents \
   '{ "Adjust Size..." = "@~s"; "Hide Sidebar" = "@1"; Thumbnails = "@2"; }'
 defaults write com.google.Chrome NSUserKeyEquivalents '{ Extensions = "@~e"; }'
 defaults write com.monosnap.monosnap NSUserKeyEquivalents '{ "Save" = "@$s"; }'
+defaults write com.apple.Numbers NSUserKeyEquivalents '{ "CSV..." = "@$e"; }'
 
 ############
 ### Dock ###
